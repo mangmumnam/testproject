dfa168a4b5c0479684263af420d9da5b2f53fb45
Lomphong <mangmumnam@gmail.com>
2018-11-17 15:29:57 +0700

    Bitmap OK

--- app/src/main/java/lomphong/androidthai/in/th/educationaccount/RegisterFragment.java
+++ app/src/main/java/lomphong/androidthai/in/th/educationaccount/RegisterFragment.java
@@ -2,10 +2,14 @@ package lomphong.androidthai.in.th.educationaccount;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -19,6 +23,13 @@ import android.widget.ImageView;
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
+
+//    Explicit
+    private boolean avataABoolean =true;
+    private ImageView imageView;
+    private Uri uri;
+    private String tag = "17novV1";
+
 
 
     public RegisterFragment() {
@@ -36,8 +47,33 @@ public class RegisterFragment extends Fragment {
 
     } //main method
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            avataABoolean = false;
+            uri = data.getData();
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, false);
+                imageView.setImageBitmap(bitmap1);
+
+            } catch (Exception e) {
+                Log.d(tag,"e==>" +e.toString());
+            }
+
+
+
+        } // if
+
+    }
+
     private void avatarController() {
-        ImageView imageView = getView().findViewById(R.id.imageViewAvatar);
+        imageView = getView().findViewById(R.id.imageViewAvatar);
         imageView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
